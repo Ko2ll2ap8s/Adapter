e8713a9f9b29ee9b4125c17b94accb5b47d8d17b
elena <elena@KOLLAPS>
2022-11-08 11:50:20 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace CA2
+{
+    class Based
+    {
+        public void BasedDouble(double value)
+        {
+            Console.WriteLine(value);
+        }
+        public void BasedInt(int value)
+        {
+            Console.WriteLine(value);
+        }
+        public void BasedChar(char value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+    interface ITarget
+    {
+        void ClientDouble(double value);
+        void ClientInt(int value);
+        void ClientChar(char value);
+    }
+    class Adapter : Based, ITarget
+    {
+        public void ClientDouble(double value)
+        {
+            BasedDouble(value);
+        }
+        public void ClientInt(int value)
+        {
+            BasedInt(value * 2);
+        }
+        public void ClientChar(char value)
+        {
+            for (int i = 0; i < 5; i++)
+                BasedChar(value);
+        }
+    }
+    class Client
+    {
+        private ITarget client;
+
+        public Client(ITarget _client)
+        {
+            client = _client;
+        }
+        public void Show()
+        {
+            client.ClientDouble(3.14);
+            client.ClientInt(228);
+            client.ClientChar('R');
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adapter adapter = new Adapter();
+            Client client = new Client(adapter);
+            client.Show();
+        }
+    }
+}
